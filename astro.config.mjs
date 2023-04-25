b1777a9f7c3786c2c1691e52e6baf7a1262cf32c
Abhinav Kumar <abhinav.kakku@gmail.com>
2023-04-25 12:54:18 +0530

    Building using Astro

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://abhinavkakku.github.io',
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap(), tailwind(), mdx()]
+});
