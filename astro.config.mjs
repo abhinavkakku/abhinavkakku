6283fe957df01be7de52aab20674e6944c64c5b5
abhinavkakku <abhinav.kakku@gmail.com>
2023-07-30 10:55:12 +0530

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ethicalhackx.com',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  }), sitemap(), mdx()],
+  
 });
