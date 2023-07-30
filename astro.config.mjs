d06e7cc56d74524238514067452b6d40130de18f
abhinavkakku <abhinav.kakku@gmail.com>
2023-07-30 19:14:32 +0530

    remarktoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import image from "@astrojs/image";
 
 import sitemap from "@astrojs/sitemap";
 
+import astroRemark from '@astrojs/markdown-remark';
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
@@ -12,5 +17,10 @@ export default defineConfig({
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap(), mdx()],
+  remarkPlugins: [ [remarkToc, { heading: ['h1', 'h2', 'h3']} ] ],
+  rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
   
 });
+
+
+
